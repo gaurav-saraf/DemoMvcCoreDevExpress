1853c207a4df485452277816e665ed2ef9643852
gaurav-saraf <gaurav.saraf31@gmail.com>
2018-08-04 23:07:49 +0530

    Code files

---
+++ Test200Core/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test200Core.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string EmailAddress { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+
+    
+}
---
+++ Test200Core/Models/CustomerData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test200Core.Models
+{
+    public class CustomerData
+    {
+        public static List<Customer> GetCustomers()
+        {
+            List<Customer> customers = new List<Customer>();
+            Customer customer = null;
+            for (int i = 1; i <= 100; i++)
+            {
+                customer =
+                    new Customer
+                    {
+                        CustomerId = i,
+                        Name = "Customer " + i.ToString(),
+                        EmailAddress = "emailaddress" + i.ToString() + ".gmail.com",
+                        City = "City " + i.ToString(),
+                        CreatedOn = new DateTime(2018, 08, 04)
+                    };
+                customers.Add(customer);
+            }
+            return customers;
+        }
+    }
+}
